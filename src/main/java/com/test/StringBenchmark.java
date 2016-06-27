2a5ad91a006bccd434e7aef693fc8b4a67c1fe7d
kjnam <kyujnam@gmail.com>
2016-06-27 23:46:55 +0900

    Update test setting

--- src/main/java/com/test/StringBenchmark.java
+++ src/main/java/com/test/StringBenchmark.java
@@ -1,27 +1,68 @@
 package com.test;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by kjnam on 2016. 6. 27..
  */
 public class StringBenchmark {
 
-    @Benchmark @BenchmarkMode(Mode.Throughput)
+    public static void main(String[] args) throws Exception {
+
+//        Basic Settings
+//
+//        # VM options: <none>
+//        # Warmup: 5 iterations, 1 s each
+//        # Measurement: 5 iterations, 1 s each
+//        # Timeout: 10 min per iteration
+//        # Threads: 1 thread, will synchronize iterations
+//        # Benchmark mode: Throughput, ops/time
+//        # Benchmark: org.sample.MyBenchmark.testMethod
+//        ...
+//        # Fork: 1 of 1
+
+        Options opt = new OptionsBuilder()
+                // Specify which benchmarks to run.
+                // You can be more specific if you'd like to run only one benchmark per test.
+                .include(StringBenchmark.class.getSimpleName())
+                // Set the following options as needed
+                .mode (Mode.AverageTime)
+                .timeUnit(TimeUnit.MICROSECONDS)
+                .warmupTime(TimeValue.seconds(1))
+                .warmupIterations(5)
+                .measurementTime(TimeValue.seconds(1))
+                .measurementIterations(5)
+                .threads(1)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                //.jvmArgs("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintInlining")
+                //.addProfiler(WinPerfAsmProfiler.class)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
     public String testEmptyBuffer() {
         StringBuffer buffer = new StringBuffer();
         return buffer.toString();
     }
 
-    @Benchmark @BenchmarkMode(Mode.Throughput)
+    @Benchmark
     public String testEmptyBuilder() {
         StringBuilder builder = new StringBuilder();
         return builder.toString();
     }
 
-    @Benchmark @BenchmarkMode(Mode.Throughput)
+    @Benchmark
     public String testEmptyLiteral() {
         return "";
     }
